507c7dc63df8441941666c1bc155c2a8dbeef0d7
Abhimanyu Singh <singhabhimanyu9838@gmail.com>
2026-01-30 04:54:34 +0530

    init

--- frontend/src/environment.js
+++ frontend/src/environment.js
@@ -1,8 +1,7 @@
 let IS_PROD = true;
 const server = IS_PROD ?
-    "https://apnacollegebackend.onrender.com" :
-
-    "http://localhost:8000"
+    "https://meetme-backend-h1vb.onrender.com" :
+    "http://localhost:8000";
 
 
 export default server;
